5f8257d48d3a254182d10f6044c4376287a68a7d
Abdulhamid Almamar <engabdulhamidalmamar@gmail.com>
2026-02-04 21:32:55 +0000

    هل نستطيع عملها هنا

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.flashwallet.eng',
-  appName: 'Flash Wallet', // يمكنك تغيير 'out' لاسم تطبيقك الحقيقي هنا
-  webDir: 'out'            // هذا هو التعديل الأهم
+  appName: 'Flash Wallet',
+  webDir: 'out',
+  bundledWebRuntime: false
 };
 
 export default config;
